d9edfc04a94e81551e1b47e4a707eb6577c80240
Yoann RADENAC <yoann.radenac@gmail.com>
2022-10-16 11:16:16 +0200

    Tutorial done: CRUD on articles and comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  root "articles#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :articles do
+    resources :comments
+  end
 
-  get "/articles", to: "articles#index"
 end
